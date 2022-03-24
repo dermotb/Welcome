774ffc412ccfdddec615dc6ec9d1db167245cb04
Dermot Boyle <dermotb@hotmail.com>
2022-03-24 22:12:26 +0000

    Initial commit for Thursday class

---
+++ Welcome/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Welcome.Models;
+
+namespace Welcome.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Welcome()
+        {
+            ViewBag.Message = "Date: " + DateTime.Now.ToShortDateString() + " - Time: " + DateTime.Now.ToShortTimeString();
+            return View();
+        }
+    }
+}
---
+++ Welcome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Welcome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
